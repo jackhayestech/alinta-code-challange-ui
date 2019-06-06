a844196f45732b91c2527d1add649041d2e61b1c
jack <jckhayes1@gmail.com>
2019-06-06 22:53:07 +1000

    Add customer data test.

--- src/redux/tests/Customers.test.ts
+++ src/redux/tests/Customers.test.ts
@@ -1,15 +1,34 @@
 import store from '../../assets/utilities/store';
 
-import { loadCustomers } from '../action_creators/Customers.actioncreator';
-import { localCustomerData } from '../../assets/data/customer.data';
+import {
+  loadCustomers,
+  addCustomer,
+} from '../action_creators/Customers.actioncreator';
+import {
+  loadCustomerData,
+  addCustomerData,
+  addedCustomerData,
+} from './data/customer.data';
 
 /**
  * Test loading customers
  *
  * Expect state to equal default
+ * Expect state to equal loacl customer data
+ *
  */
-test('should fail loading property data', async () => {
+test('should load customer data', async () => {
   expect(store.getState().Customers.data).toEqual([]);
-  store.dispatch<any>(loadCustomers(localCustomerData));
-  expect(store.getState().Customers.data).toEqual(localCustomerData);
+  store.dispatch<any>(loadCustomers(loadCustomerData));
+  expect(store.getState().Customers.data).toEqual(loadCustomerData);
+});
+
+/**
+ * Test adding customer
+ *
+ * Expect state to equal default
+ */
+test('should add customer data', async () => {
+  store.dispatch<any>(addCustomer(addCustomerData));
+  expect(store.getState().Customers.data).toEqual(addedCustomerData);
 });
---
+++ src/redux/tests/data/customer.data.ts
@@ -0,0 +1,42 @@
+export const loadCustomerData = [
+  {
+    id: 1,
+    firstName: 'fn1',
+    lastName: 'ln1',
+    dob: '2019-01-01',
+  },
+  {
+    id: 2,
+    firstName: 'fn2',
+    lastName: 'ln2',
+    dob: '2019-01-02',
+  },
+];
+
+export const addCustomerData = {
+  id: 3,
+  firstName: 'fn3',
+  lastName: 'ln3',
+  dob: '2019-01-03',
+};
+
+export const addedCustomerData = [
+  {
+    id: 1,
+    firstName: 'fn1',
+    lastName: 'ln1',
+    dob: '2019-01-01',
+  },
+  {
+    id: 2,
+    firstName: 'fn2',
+    lastName: 'ln2',
+    dob: '2019-01-02',
+  },
+  {
+    id: 3,
+    firstName: 'fn3',
+    lastName: 'ln3',
+    dob: '2019-01-03',
+  },
+];
