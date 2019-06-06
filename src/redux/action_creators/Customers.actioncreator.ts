9ab63a9d3787e278ec3fbd0f6ab170f767677a08
jack <jckhayes1@gmail.com>
2019-06-06 22:40:45 +1000

    Created add and delete action.

--- src/redux/action_creators/Customers.actioncreator.ts
+++ src/redux/action_creators/Customers.actioncreator.ts
@@ -1,10 +1,24 @@
 import { Customer } from '../../assets/interfaces'
 import { Dispatch } from 'redux'
-import { LOAD_CUSTOMERS } from '../Actions';
+import { LOAD_CUSTOMERS, ADD_CUSTOMER, DELETE_CUSTOMER } from '../Actions';
 
-// Loads property data
+// Loads customer data
 export const loadCustomers = (customers: Customer[]) => (dispatch: Dispatch) =>
   dispatch({
     type: LOAD_CUSTOMERS,
     data: customers,
+  });
+
+// Adds customer data
+export const addCustomer = (customer: Customer) => (dispatch: Dispatch) =>
+  dispatch({
+    type: ADD_CUSTOMER,
+    data: customer,
+  });
+
+// Deletes customer data
+export const deleteCustomer = (index: number) => (dispatch: Dispatch) =>
+  dispatch({
+    type: DELETE_CUSTOMER,
+    data: index,
   });
