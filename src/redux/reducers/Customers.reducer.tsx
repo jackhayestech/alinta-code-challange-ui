bf27d88850c326fb60b50174a67adb09dfe06831
jack <jckhayes1@gmail.com>
2019-06-06 21:56:20 +1000

    Added load customers action.

--- src/redux/reducers/Customers.reducer.tsx
+++ src/redux/reducers/Customers.reducer.tsx
@@ -2,33 +2,38 @@ import { action } from './Root.reducer';
 import { localCustomerData } from '../../assets/data/customer.data';
 
 // If the application is being run without connecting to an api load local customer data.
-let customerData: object[] = process.env.REACT_APP_LOCAL_DEV ? localCustomerData : [];
+let customerData: object[] = process.env.REACT_APP_LOCAL_DEV
+  ? localCustomerData
+  : [];
 
 const initialState = {
   data: customerData,
 };
 
+export const LOAD_CUSTOMERS = 'LOAD_CUSTOMERS';
 export const ADD_CUSTOMER = 'ADD_CUSTOMER';
 export const DELETE_CUSTOMER = 'DELETE_CUSTOMER';
 
 const Customers = (state = initialState, action: action) => {
   switch (action.type) {
+    case LOAD_CUSTOMERS:
+      return {
+        ...state,
+        data: action.data,
+      };
     case ADD_CUSTOMER:
       return {
         ...state,
-        data: [
-          ...state.data,
-          action.data,
-        ]
-      }
+        data: [...state.data, action.data],
+      };
     case DELETE_CUSTOMER:
       return {
         ...state,
         data: [
           ...state.data.slice(0, action.data),
-          ...state.data.slice(action.data + 1)
-        ]
-      }
+          ...state.data.slice(action.data + 1),
+        ],
+      };
     default:
       return state;
   }
