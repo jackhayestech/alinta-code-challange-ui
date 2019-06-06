be628df786c910388556d0aa4e8733e94af63f9f
jack <jckhayes1@gmail.com>
2019-06-06 21:05:47 +1000

    Added redux to application.

---
+++ src/assets/utilities/store.tsx
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import createRootReducer from '../../redux/reducers/Root.reducer';
+
+const middlewareCompose = compose(
+  applyMiddleware(
+    thunk,
+  ),
+);
+
+export const composeEnhancers =
+  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(
+  createRootReducer(),
+  composeEnhancers(middlewareCompose),
+);
+
+export default store;
