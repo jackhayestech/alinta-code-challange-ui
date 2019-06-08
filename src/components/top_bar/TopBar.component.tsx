a04dd62fb2cd9fecbfc6ae51bdc846f0ebcb3df4
jack <jckhayes1@gmail.com>
2019-06-08 21:04:31 +1000

    Add customer component.

--- src/components/top_bar/TopBar.component.tsx
+++ src/components/top_bar/TopBar.component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Search from './search/Search.connector';
+import AddCustomer from './add_customer/AddCustomer.component';
 
 export const TopBar: React.FunctionComponent = (): JSX.Element => (
   <div id="topBar">
     <Search />
+    <AddCustomer />
   </div>
 );
 
---
+++ src/components/top_bar/add_customer/AddCustomer.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AddCustomer: React.FunctionComponent = (): JSX.Element => (
+  <div>
+    Add customer.
+  </div>
+);
+
+export default AddCustomer;
