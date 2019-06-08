f88dfe3d02c450bcf3a9491634982223f5f2ba4d
jack <jckhayes1@gmail.com>
2019-06-08 22:03:40 +1000

    Styled modal and body.

--- src/components/modal/Modal.component.tsx
+++ src/components/modal/Modal.component.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+  position: absolute;
+  cursor: pointer;
+`;
 
 interface Modal {
   display: boolean;
   toggle: Function;
 }
 
-export const Modal: React.FC<Modal> = ({ display, toggle }) => (
+export const Modal: React.FC<Modal> = ({ display, toggle }) =>
   display ? (
-    <div>modal</div>
-  ) : null
-);
+    <Background
+      onClick={() => {
+        toggle();
+      }}
+    >
+      modal
+    </Background>
+  ) : null;
 
 export default Modal;
