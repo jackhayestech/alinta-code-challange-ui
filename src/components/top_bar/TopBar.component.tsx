331424c05b1b219d334c4d0043273ffd3707baca
jack <jckhayes1@gmail.com>
2019-06-08 12:26:38 +1000

    Search component.

--- src/components/top_bar/TopBar.component.tsx
+++ src/components/top_bar/TopBar.component.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Search from './search/Search.component';
 
 export const TopBar: React.FunctionComponent = (): JSX.Element => (
   <div id="topBar">
-    
+    <Search />
   </div>
 );
 
