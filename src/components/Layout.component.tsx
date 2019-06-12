7d20db3c459536d5cf82645a2fb100f4afc99611
jack <jckhayes1@gmail.com>
2019-06-12 12:21:02 +1000

    Styled the layout.

--- src/components/Layout.component.tsx
+++ src/components/Layout.component.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Modal from './modal/Modal.connector';
 import TopBar from './top_bar/TopBar.component';
 import Customers from './customers/Customers.connector';
 
+const LayoutContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  margin: 50px;
+`;
+
 export const Layout: React.FunctionComponent = (): JSX.Element => (
-  <div id="layout">
+  <LayoutContainer>
     <Modal />
     <TopBar />
     <Customers />
-  </div>
+  </LayoutContainer>
 );
 
 export default Layout;
