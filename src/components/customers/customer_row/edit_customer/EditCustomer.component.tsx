ab1e416087772f42cc58e7ada396edc8a65d4741
jack <jckhayes1@gmail.com>
2019-06-12 11:07:55 +1000

    Open edit customer component.

--- src/components/customers/customer_row/edit_customer/EditCustomer.component.tsx
+++ src/components/customers/customer_row/edit_customer/EditCustomer.component.tsx
@@ -4,15 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 interface EditCustomer {
-  edit: Function;
-  custIndex: number;
+  toggleModalDisplay: Function;
+  setModalType: Function;
+  setSelectedCustomer: Function;
 }
 
-export const EditCustomer: React.FC<EditCustomer> = ({ edit, custIndex }): JSX.Element => (
+export const EditCustomer: React.FC<EditCustomer> = ({ toggleModalDisplay, setModalType, setSelectedCustomer }): JSX.Element => (
   <div
     className="button-icon"
     onMouseDown={() => {
-      edit(custIndex);
+      toggleModalDisplay();
+      setModalType();
+      setSelectedCustomer();
     }}
     role="button"
     tabIndex={0}
